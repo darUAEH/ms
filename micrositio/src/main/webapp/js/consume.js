fa52d593f070c35246240c89b8d66c53068b001c
Desarrollo <dar_ti@uaeh.edu.mx>
2022-02-02 12:25:37 -0600

    subiendo

--- micrositio/src/main/webapp/js/consume.js
+++ micrositio/src/main/webapp/js/consume.js
@@ -2,14 +2,34 @@
 $(document).ready(function() {
 	console.log("ready!");
 	const inicio = "/micrositio/index.html";
+	const indice = "/micrositio/";
+	const reflexion = "/micrositio/reflexion.html";
 
 	readTextFile("js/textos.json", function(text) {
 		var data = JSON.parse(text);
 		console.log(data);
 		datap = data;
-		if (window.location.pathname === inicio) {
-			$('#seccion').text(data["Variables"]["index"].seccion);
-			$('#cuerpo').html(data["Variables"]["index"].cuerpo);
+	
+		for(let i = 0; i < Object.keys(datap["menu_izq"]).length; i++)
+		{
+			let html= '<li><a href="'+data['menu_izq'][i].href+'">'+data['menu_izq'][i].texto+
+            '<span class="'+data['menu_izq'][i].class0+'">'+
+ 			'	<i class="'+data['menu_izq'][i].class1+'"></i>'+	
+ 			'	<i class="'+data['menu_izq'][i].class2+'"></i> '+
+ 			'</span></a></li>'
+			
+			$('#menu_izq').append(html)
+			
+		}		
+		if ((window.location.pathname === inicio)||(window.location.pathname === indice)) {
+			(data["Variables"]["index"]["seccion"][0] ==="text") ? $('#seccion').text(data["Variables"]["index"]["seccion"][1]) : $("#seccion").load(data["Variables"]["index"]["seccion"][1]);     
+			(data["Variables"]["index"]["cuerpo"][0] ==="text") ? $('#cuerpo').text(data["Variables"]["index"]["cuerpo"][1]) : $("#cuerpo").load(data["Variables"]["index"]["cuerpo"][1]);  
+			
+		}
+		if (window.location.pathname === reflexion) {
+			(data["Variables"]["reflexion"]["seccion"][0] ==="text") ? $('#seccion').text(data["Variables"]["reflexion"]["seccion"][1]) : $("#seccion").load(data["Variables"]["reflexion"]["seccion"][1]);     
+			(data["Variables"]["reflexion"]["cuerpo"][0] ==="text") ? $('#cuerpo').text(data["Variables"]["reflexion"]["cuerpo"][1]) : $("#cuerpo").load(data["Variables"]["reflexion"]["cuerpo"][1]);  
+			
 		}
 		
 	});
